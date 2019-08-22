90a90f9e8894aa497a79ad397a977b3f6a79395f
EUROPE\pmperes <pedromiguel.peres@altran.com>
2019-08-22 17:48:30 +0100

    Add WPF Project with MVVM

---
+++ CWDesktopUI/Bootstrapper.cs
@@ -0,0 +1,60 @@
+﻿using Caliburn.Micro;
+using CWDesktopUI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CWDesktopUI
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        private SimpleContainer _container = new SimpleContainer();
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void Configure()
+        {
+            _container.Instance(_container);
+
+            _container
+                .Singleton<IWindowManager, WindowManager>()
+                .Singleton<IEventAggregator, EventAggregator>();
+
+            // get every type in my application
+            GetType().Assembly.GetTypes()
+                // of type class
+                .Where(type => type.IsClass)
+                // where name of the class ends with "ViewModel"
+                .Where(type => type.Name.EndsWith("ViewModel"))
+                // make a list of the classes
+                .ToList()
+                // iterate the list
+                .ForEach(viewModelType => _container.RegisterPerRequest(viewModelType, viewModelType.ToString(), viewModelType));
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            return _container.GetInstance(service, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _container.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            _container.BuildUp(instance);
+        }
+    }
+}
